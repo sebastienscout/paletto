40f69d8a3f66360bd5223a327f971d1d486615e5
esailly <esailly@free.fr>
2016-10-11 14:25:30 +0200

    3.2 Test joueur 1 choisi bille jaune

--- test/test.js
+++ test/test.js
@@ -8,7 +8,14 @@ PalettoTestCase.prototype.testStory1 = function () {
     'use strict';
     assertTrue(e.get_marble_color("c3") === 'blue');
     assertTrue(e.get_marble_color("e5") === 'yellow');
-
     assertTrue(e.verify_juxtaposition() === true);
+
+};
+
+PalettoTestCase.prototype.testStory2 = function () {
+    'use strict';
+
+    assertTrue(e.current_player() === 1);
+    assertTrue(e.play('yellow') === true);
 
 };
