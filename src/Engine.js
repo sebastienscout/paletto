8cf109cbd2ea651a33a66e7e2b33e3c489c10503
esailly <esailly@free.fr>
2016-10-11 13:51:04 +0200

    3.1 Test positionné code à faire

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,37 @@
-'use strict';
+
 
 var Engine = function () {
+    "use strict";
+    var grid = [6];
+    var row;
+    var col;
+    //               0        1        2        3      4       5
+    var colors = ['black', 'green', 'white', 'blue', 'red', 'yellow'];
 
+    var constructor = function(){
+        for (row = 0; row < 6; row ++) {
+            grid[row] = [6];
+        }
+        var colorsTemp = [0,1,2,3,4,2,5,2,1,4,5,3,3,5,3,2,0,4,4,0,4,1,3,2,2,1,5,0,5,1,5,3,0,4,1,0];
+
+
+        for (row = 0; row < 6; row ++) {
+            for (col = 0; col < 6; col ++) {
+                grid[row][col] = colors[colorsTemp[row*6+col]];
+            }
+        }
+
+    };
+    constructor();
+
+    this.get_marble_color = function (position) {
+        col = position.charCodeAt(0) - "a".charCodeAt(0);
+        row = Number(position.charAt(1)) - 1;
+        return grid[row][col];
+    };
 // private attributes and methods
+
+
 
 // public methods
 };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,14 @@
-'use strict';
+
 
 var PalettoTestCase = TestCase("PalettoTestCase");
 
+var e = new Engine();
+
 PalettoTestCase.prototype.testStory1 = function () {
+    'use strict';
+    assertTrue(e.get_marble_color("c3") === 'blue');
+    assertTrue(e.get_marble_color("e5") === 'yellow');
+
+    assertTrue(e.verify_juxtaposition() === true);
+
 };
