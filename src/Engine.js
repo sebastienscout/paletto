1cfd5ea08b651887c2c0377af55ee30f1e93cc83
esailly <esailly@free.fr>
2016-10-18 09:16:47 +0200

    3.4 Implementation joueur joue black sur a1 et f6

--- src/Engine.js
+++ src/Engine.js
@@ -20,7 +20,6 @@ var Engine = function () {
         }
         var colorsTemp = [0,1,2,3,4,2,5,2,1,4,5,3,3,5,3,2,0,4,4,0,4,1,3,2,2,1,5,0,5,1,5,3,0,4,1,0];
 
-
         for (row = 0; row < 6; row ++) {
             for (col = 0; col < 6; col ++) {
                 grid[row][col] = colors[colorsTemp[row*6+col]];
@@ -35,6 +34,19 @@ var Engine = function () {
         row = Number(position.charAt(1)) - 1;
         return grid[row][col];
     };
+
+    this.set_marble_color = function (_position, _color) {
+        col = _position.charCodeAt(0) - "a".charCodeAt(0);
+        row = Number(_position.charAt(1)) - 1;
+        grid[row][col] = _color;
+    };
+
+    this.get_pos_from_grid = function(_row, _col){
+        var col_letter = String.fromCharCode(97 + _col);
+        return (col_letter + (_row+1).toString());
+    };
+
+
 
     this.verify_juxtaposition = function() {
         var test = 0;
@@ -51,27 +63,17 @@ var Engine = function () {
         return current_player_id;
     };
 
+
     this.play = function(color){
         var test = false;
+        var num_play;
 
-        if(grid[0][0] === color) {
+        var one_play = this.can_play(color);
+        for (num_play = 0; num_play < one_play.length; num_play++) {
+            this.set_marble_color(one_play[num_play],'empty');
+            players [current_player_id] [colors.indexOf(color)] ++;
             test = true;
-            grid[0][0] = 'empty';
         }
-        else if(grid[5][0] === color) {
-            test = true;
-            grid[5][0] = 'empty';
-        }
-        else if(grid[0][5] === color) {
-            test = true;
-            grid[0][5] = 'empty';
-        }
-        else if(grid[5][5] === color) {
-            test = true;
-            grid[5][5] = 'empty';
-        }
-
-        players [current_player_id] [colors.indexOf(color)] ++;
 
         current_player_id = (current_player_id+1)%2;
 
@@ -90,4 +92,30 @@ var Engine = function () {
     this.get_marble_player = function(player,color) {
          return players[player] [colors.indexOf(color)];
     };
+
+    this.is_valid = function(row, col){
+        var nb_neighbor = 0;
+
+        nb_neighbor += (row < 5 && grid[row+1][col] !== 'empty');
+        nb_neighbor += (row > 0 && grid[row-1][col] !== 'empty');
+        nb_neighbor += (col < 5 && grid[row][col+1] !== 'empty');
+        nb_neighbor += (col > 0 && grid[row][col-1] !== 'empty');
+        return nb_neighbor < 3;
+    };
+
+    this.can_play = function(color){
+        var pos_colors = [];
+        var i = 0;
+        for (row = 0; row < 6; row ++) {
+            for (col = 0; col < 6; col ++) {
+                if(grid[row][col] === color && this.is_valid(row, col)){
+                    pos_colors[i] = this.get_pos_from_grid(row, col);
+                    i++;
+                }
+            }
+        }
+        return pos_colors;
+    };
+
+
 };
