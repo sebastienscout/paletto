c12fe1110fccc42b50cb7f6b240119a591f2abf1
esailly <esailly@free.fr>
2016-10-22 14:55:41 +0200

    3.9 test jeu a 8*8 joue aleatoirement et cherche un gagnant !

--- test/test64.js
+++ test/test64.js
@@ -21,6 +21,20 @@ PalettoTestCase64.prototype.testStory64_1 = function () {
     }
 
     e.print_log();
+ };
+
+PalettoTestCase64.prototype.testStory64_2 = function () {
+    'use strict';
+    var test_color;
+    var cpt = 0;
+    while (e.number_marble() > 0 && e.test_winner() < 0 && cpt < 100) {
+        test_color = Math.floor(Math.random() * 8);
+        e.play(e.get_color[test_color]);
+        cpt++; //evite boucle infinie
+    }
+    e.print_log();
+    assertTrue(e.test_winner() >= 0);
+
 };
 
 
